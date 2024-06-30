9e05c623b7c6aabff1c0ed7dcaa7ee33a908eb43
Amber Sinha <114065662+Ambersinha97@users.noreply.github.com>
2024-06-30 21:32:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function toCase(text) {
   // write your code here
+	let str=""
+	for(let i=0;i<text.length;i++){
+		if(text.charCodeAt(i)>=65 && text.charCodeAt(i)<=91)
+			str+=String.fromCharCode(text.charCodeAt(i)+32)
+			else
+			str+=text.charAt(i)
+	}
+	str+="-"
+	for(let i=0;i<text.length;i++){
+		if(text.charCodeAt(i)>=97 && text.charCodeAt(i)<=123)
+			str+=String.fromCharCode(text.charCodeAt(i)-32)
+			else
+			str+=text.charAt(i)
+	}
 }
 
 // DO not change the code below
